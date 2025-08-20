eff42ae32059e6308e95f5e0cfba6fe8a5b350a3
Lucas Godoy <lucasgodoyjor@gmail.com>
2025-08-20 17:19:35 -0300

    Exercicio 3.5 imprimir quadrado de caractere informado pelo usuário

--- 3.0-funcoes/3.5-padraodeasteriscos/3.5-padraodeasteriscos.c
+++ 3.0-funcoes/3.5-padraodeasteriscos/3.5-padraodeasteriscos.c
@@ -1,3 +1,28 @@
 /*
 Crie uma função que imprime um quadrado de asteriscos com lado n. O usuário dirá qual o valor de n e qual caracter será usado (*, #, $ ...).
 */
+
+#include <stdio.h>
+
+void imprimeQuadrado(int n, char c) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%c", c);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int n;
+    char c;
+
+    printf("Digite o tamanho do lado do quadrado: ");
+    scanf("%d", &n);
+    printf("Digite o caracter a ser usado: ");
+    scanf(" %c", &c);
+
+    imprimeQuadrado(n, c);
+
+    return 0;
+}
