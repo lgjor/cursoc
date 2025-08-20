d9f07eba96ac4863ebb68981c7695f48fb0a3176
Lucas Godoy <lucasgodoyjor@gmail.com>
2025-08-20 17:13:41 -0300

    Exercicio 3.3 retorna o maior numero entre 3

--- 3.0-funcoes/3.3-maiorde3numeros/3.3-maiorde3numeros.c
+++ 3.0-funcoes/3.3-maiorde3numeros/3.3-maiorde3numeros.c
@@ -1,3 +1,34 @@
 /*
 Crie uma função que receba três inteiros e retorne o maior deles.
 */
+
+#include <stdio.h>
+
+int maiorDeTres(int a, int b, int c){
+    int maior = a; // Assume a é o maior inicialmente
+
+    if (b > maior) {
+        maior = b; // Atualiza se b for maior
+    }
+    if (c > maior) {
+        maior = c; // Atualiza se c for maior
+    }
+
+    return maior; // Retorna o maior valor encontrado
+}
+
+int main() {
+    int num1, num2, num3;
+
+    printf("Digite o primeiro numero: ");
+    scanf("%d", &num1);
+    printf("Digite o segundo numero: ");
+    scanf("%d", &num2);
+    printf("Digite o terceiro numero: ");
+    scanf("%d", &num3);
+
+    int maior = maiorDeTres(num1, num2, num3);
+    printf("O maior numero entre %d, %d e %d é: %d\n", num1, num2, num3, maior);
+
+    return 0;
+}
