b26cde1a6eb31cf3a443abaf20797c00447f83b2
Lucas Godoy <lucasgodoyjor@gmail.com>
2025-08-20 17:16:56 -0300

    exercício 3.4 - classificar nota

--- 3.0-funcoes/3.4-classificarnotas/3.4-classificarnotas.c
+++ 3.0-funcoes/3.4-classificarnotas/3.4-classificarnotas.c
@@ -5,3 +5,26 @@ Aprovado (nota >= 6)
 Recuperação (nota >= 4 e < 6)
 Reprovado (nota < 4)
 */
+
+#include <stdio.h>
+
+void classificarNota(float nota) {
+    if (nota >= 6) {
+        printf("Aprovado\n");
+    } else if (nota >= 4) {
+        printf("Recuperação\n");
+    } else {
+        printf("Reprovado\n");
+    }
+}
+
+int main() {
+    float nota;
+
+    printf("Digite a nota do aluno: ");
+    scanf("%f", &nota);
+
+    classificarNota(nota);
+
+    return 0;
+}
